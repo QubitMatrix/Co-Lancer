14a4d8ff1b2a5495f11b7a7f70cab1a52e8f2392
QubitMatrix <preethi13m@gmail.com>
2023-11-03 17:17:13 +0530

    project details file upload and display codes

---
+++ source code/co-lancer/src/pages/file.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+
+function FileUpload(props) 
+{ 
+    const [file, setFile] = useState(null);
+    
+    //Handle adding new file
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        console.log(selectedFile); 
+        setFile(selectedFile);
+    }
+
+    //Handle uploading of file
+    const handleUpload = (e) => {
+        e.preventDefault();
+
+        if(file)
+        {
+            //FormData object to store file details
+            const formData = new FormData();
+            formData.append('pdf', file);
+            formData.append('project_title', props.project_title)
+
+            //Post the file to backend to save into database
+            fetch('http://localhost:3000/upload_pdf', {
+            method: 'POST',
+            body: formData,
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                alert('PDF uploaded successfully.');
+            })
+            .catch((error) => {
+                console.error(error);
+                alert('PDF upload failed.');
+            });
+        }
+
+        else
+        {
+            alert("choose a file");
+        }
+
+    }
+
+    //Render file upload
+    return (
+        <div>
+            <input type="file" name="requirements" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+    );
+}
+
+export default FileUpload;
---
+++ source code/co-lancer/src/pages/file_display.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const PdfDisplay = () => {
+  //State variable for pdf data
+  const [pdfData, setPdfData] = useState(null);
+
+  //use states from previous component
+  const {state} = useLocation();
+  console.log("state"+state.title)
+
+  useEffect(() => {
+    // Replace 'pdfId' with the ID of the PDF you want to display
+    const pdfId = state["title"]; // Change this to the desired PDF ID
+    console.log(state)
+
+    //Hit backend to retrieve file data
+    fetch(`http://localhost:3000/display_pdf/${pdfId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        const pdfUrl = URL.createObjectURL(blob);
+        setPdfData(pdfUrl);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  //Render the pdf in a iframe
+  return (
+    <div>
+      <h2>PDF Display</h2>
+      {pdfData && (
+        <iframe
+          src={pdfData}
+          title="Uploaded PDF"
+          width="100%"
+          height="500px"
+        />
+      )}
+    </div>
+  );
+};
+
+export default PdfDisplay;
